077c7c38fa53be3367eefcb955b345ae28510554
Matheus Link <linksauermths2203@gmail.com>
2024-02-10 22:48:40 -0300

    styles: estilizando nav

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -2,29 +2,33 @@ import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import { useState } from "react";
 
+import "./style.scss";
+
 const NavBar = () => {
   const [search, setSearch] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    
-    if(!search){
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) {
       alert("You need to type something");
       return;
     }
 
-    navigate(`/search?q=${search}`)
-    setSearch("")
-  }
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
   return (
     <nav id="navbar">
-      <h2>
-        <Link to="/">
-          <BiCameraMovie /> MovieVerse
-        </Link>
-      </h2>
-      <form onSubmit={handleSubmit}>
+      <section>
+        <h2>
+          <Link to="/">
+            <BiCameraMovie /> MovieVerse
+          </Link>
+        </h2>
+      </section>
+      <form onSubmit={handleSubmit} className="nav-form">
         <input
           type="text"
           placeholder="Search the movies"
